10493db7e83ae74a821a94d6efd29157169c0db8
Sebastian Estrada <Sebastian34@berkeley.edu>
2025-06-13 19:22:29 -0700

    Added Working signup page

--- carlog-frontend/src/App.tsx
+++ carlog-frontend/src/App.tsx
@@ -1,16 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Auth from "./pages/Auth";
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="*" element={<Login />} />
+      <Route path="*" element={<Auth />} />
       <Route path="/auth" element={<Auth />} />
     </Routes>
   );
